59cb1a81b9db43118f9682b9a5f5674272b86919
Spencer Lee Meredith <16077691+SLM-Dev@users.noreply.github.com>
2021-04-18 17:52:21 +0000

    to be continue

--- BNB_Using_Nuxt_/pages/home/_id.vue
+++ BNB_Using_Nuxt_/pages/home/_id.vue
@@ -17,7 +17,7 @@ export default {
         return {
             home : {}
         }
-    },
+    },                                              
     created(){
         const home = homes.find((home) => home.objectID == this.$route.params.id)
         this.home = home
